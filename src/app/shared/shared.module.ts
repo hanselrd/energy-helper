a446c5371df9726571c38d2140c1188f1a5358c5
Slenderize <Chhayhout_chhoeu@student.uml.edu>
2017-11-04 21:47:45 -0400

    third commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '@app/material';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   imports: [
@@ -12,11 +14,14 @@ import { MaterialModule } from '@app/material';
     RouterModule,
     MaterialModule
   ],
-  declarations: [],
+  declarations: [
+    FooterComponent
+  ],
   exports: [
     ReactiveFormsModule,
     RouterModule,
-    MaterialModule
+    MaterialModule,
+    FooterComponent
   ]
 })
 export class SharedModule { }
